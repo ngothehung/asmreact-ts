4d96693cd012ecf001e4580da1a656864d9ade55
ngothehung <ngothehung2110@gmail.com>
2023-04-11 21:28:27 +0700

    api

--- src/pages/admin/AddProduct.tsx
+++ src/pages/admin/AddProduct.tsx
@@ -8,37 +8,7 @@ interface IProps {
     onAdd: (product: IProduct) => void
 }
 
-// const AddProductPage = (props) => { // nhận props từ App.tsx 
-//     // props - onAdd
-//     const navigate = useNavigate() // khởi tạo navigate để điều hướng
-//     const [inputValue, setInputValue] = useState({}) // khởi tạo state để lưu giá trị input
-//     const onHandleChange = (e) => { // hàm xử lý sự kiện khi input thay đổi
-//         const name = e.target.name;
-//         const value = e.target.value;
-//         setInputValue({ ...inputValue, [name]: value });
-//         // n -> n
-//         // e -> e
 
-//         // setInputValue({ name: e.target.value }); // set giá trị cho state
-
-//     }
-//     const onHandleSubmit = (e) => { // hàm xử lý sự kiện khi submit form
-//         e.preventDefault()
-//         // console.log(inputValue);
-
-//         props.onAdd(inputValue); // gọi hàm onAdd từ props truyền vào
-//         navigate('/admin/products') // điều hướng đến trang /admin/products
-//     }
-//     return (
-//         <div>
-//             <form action="" onSubmit={onHandleSubmit}>
-//                 <input type="text" placeholder='Product Name' onChange={onHandleChange} name='name' />
-//                 <input type="number" onChange={onHandleChange} name='price' />
-//                 <button type="submit">Add New Product</button>
-//             </form>
-//         </div>
-//     )
-// }
 
 interface IProps {
     onAdd: (product: IProduct) => void
